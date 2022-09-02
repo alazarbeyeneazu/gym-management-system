8738bc515377f1b45a1a1585b21a5dfc8a9cb5b3
alazarbeyene <alazarbeyene99@gmail.com>
2022-09-02 16:47:30 +0300

    modifying test for filtering user  to make it better and use mockdb

--- internal/module/user/filteruser_test.go
+++ internal/module/user/filteruser_test.go
@@ -2,30 +2,23 @@ package user
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"math/rand"
 	"testing"
+	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/2ftimeplc/2fbackend/delivery-1/internal/constants/models"
+	mockdb "gitlab.com/2ftimeplc/2fbackend/delivery-1/mocks/db"
 	"gitlab.com/2ftimeplc/2fbackend/delivery-1/platform/utils"
 )
 
 func TestGetUserByFirstName(t *testing.T) {
-	appUser := Initiate()
-	fistname := utils.RandomUserName()
-	for i := 0; i < 10; i++ {
-		email := utils.RandomeEmail()
-		lastName := utils.RandomUserName()
-		phoneNumber := utils.RandomePhoneNumber()
-		user := models.User{
-			FirstName:   fistname,
-			LastName:    lastName,
-			Email:       email,
-			PhoneNumber: phoneNumber,
-			Password:    utils.RandomPassword(),
-		}
-		appUser.RegisterUser(context.Background(), user)
+	ctl := gomock.NewController(t)
+	db := mockdb.NewMockDBPort(ctl)
 
-	}
 	testCase := []struct {
 		name      string
 		firstName string
@@ -33,11 +26,11 @@ func TestGetUserByFirstName(t *testing.T) {
 	}{
 		{
 			name:      "ok",
-			firstName: fistname,
+			firstName: "firstnamehere",
 			checker: func(t *testing.T, users []models.User, err models.Errors) {
 				require.Empty(t, err)
 				require.Equal(t, len(users), 10)
-				require.Equal(t, users[0].FirstName, fistname)
+				require.Equal(t, users[0].FirstName, "firstnamehere")
 
 			},
 		},
@@ -50,40 +43,53 @@ func TestGetUserByFirstName(t *testing.T) {
 			},
 		},
 		{
-			name:      "empty first name ",
+			name:      "empty first name",
 			firstName: "",
 			checker: func(t *testing.T, users []models.User, err models.Errors) {
 				require.Empty(t, users)
+				require.Equal(t, err.Err, errors.New("first name cannot be blank"))
 
 			},
 		},
 	}
 	for _, tc := range testCase {
 		t.Run(tc.name, func(t *testing.T) {
-			accounts, err := appUser.GetUsersByFirstName(context.Background(), tc.firstName)
-			tc.checker(t, accounts, err)
+			switch tc.name {
+			case "ok":
+
+				var user []models.User
+				for i := 0; i < 10; i++ {
+					user = append(user, models.User{Id: rand.Int63(), FirstName: tc.firstName, LastName: utils.RandomUserName(), PhoneNumber: utils.RandomePhoneNumber(), Email: utils.RandomeEmail(), CreatedAt: time.Now().GoString(), State: 1})
+				}
+				db.EXPECT().GetUsersByFirstName(context.Background(), tc.firstName).Return(user, models.Errors{})
+				defer ctl.Finish()
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUsersByFirstName(context.Background(), tc.firstName)
+				tc.checker(t, accounts, err)
+			case "not found":
+				defer ctl.Finish()
+				db.EXPECT().GetUsersByFirstName(context.Background(), tc.firstName).Return([]models.User{}, models.Errors{Err: sql.ErrNoRows})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUsersByFirstName(context.Background(), tc.firstName)
+				tc.checker(t, accounts, err)
+
+			case "empty first name":
+				defer ctl.Finish()
+				db.EXPECT().GetUsersByFirstName(context.Background(), tc.firstName).Return([]models.User{}, models.Errors{Err: errors.New("not empty")})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUsersByFirstName(context.Background(), tc.firstName)
+				tc.checker(t, accounts, err)
+			}
+
 		})
 	}
 
 }
 
 func TestGetUserByLastName(t *testing.T) {
-	appUser := Initiate()
-	lastname := utils.RandomUserName()
-	for i := 0; i < 10; i++ {
-		email := utils.RandomeEmail()
-		firstname := utils.RandomUserName()
-		phoneNumber := utils.RandomePhoneNumber()
-		user := models.User{
-			FirstName:   firstname,
-			LastName:    lastname,
-			Email:       email,
-			PhoneNumber: phoneNumber,
-			Password:    utils.RandomPassword(),
-		}
-		appUser.RegisterUser(context.Background(), user)
+	ctl := gomock.NewController(t)
+	db := mockdb.NewMockDBPort(ctl)
 
-	}
 	testCase := []struct {
 		name     string
 		lastName string
@@ -91,11 +97,11 @@ func TestGetUserByLastName(t *testing.T) {
 	}{
 		{
 			name:     "ok",
-			lastName: lastname,
+			lastName: "lastnamehere",
 			checker: func(t *testing.T, users []models.User, err models.Errors) {
 				require.Empty(t, err)
 				require.Equal(t, len(users), 10)
-				require.Equal(t, users[0].LastName, lastname)
+				require.Equal(t, users[0].LastName, "lastnamehere")
 
 			},
 		},
@@ -104,31 +110,57 @@ func TestGetUserByLastName(t *testing.T) {
 			lastName: utils.RandomUserName(),
 			checker: func(t *testing.T, users []models.User, err models.Errors) {
 				require.Empty(t, users)
+				require.EqualError(t, err.Err, sql.ErrNoRows.Error())
 
 			},
 		},
 		{
-			name:     "empty first name ",
+			name:     "empty last name",
 			lastName: "",
 			checker: func(t *testing.T, users []models.User, err models.Errors) {
 				require.Empty(t, users)
+				require.EqualError(t, err.Err, "LastName cannot be blank")
 
 			},
 		},
 	}
 	for _, tc := range testCase {
 		t.Run(tc.name, func(t *testing.T) {
-			accounts, err := appUser.GetUsersByLastName(context.Background(), tc.lastName)
-			tc.checker(t, accounts, err)
+			switch tc.name {
+			case "ok":
+				var user []models.User
+				for i := 0; i < 10; i++ {
+					user = append(user, models.User{Id: rand.Int63(), FirstName: utils.RandomUserName(), LastName: tc.lastName, PhoneNumber: utils.RandomePhoneNumber(), Email: utils.RandomeEmail(), CreatedAt: time.Now().GoString(), State: 1})
+				}
+				db.EXPECT().GetUsersByLastName(context.Background(), tc.lastName).Return(user, models.Errors{})
+				defer ctl.Finish()
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUsersByLastName(context.Background(), tc.lastName)
+				tc.checker(t, accounts, err)
+			case "not found":
+				defer ctl.Finish()
+				db.EXPECT().GetUsersByLastName(context.Background(), tc.lastName).Return([]models.User{}, models.Errors{Err: sql.ErrNoRows})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUsersByLastName(context.Background(), tc.lastName)
+				tc.checker(t, accounts, err)
+
+			case "empty last name":
+				defer ctl.Finish()
+				db.EXPECT().GetUsersByLastName(context.Background(), tc.lastName).Return([]models.User{}, models.Errors{Err: errors.New("unexpected error")})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUsersByLastName(context.Background(), tc.lastName)
+				tc.checker(t, accounts, err)
+			}
 		})
 	}
 
 }
 
 func TestGetUserByPhoneNumber(t *testing.T) {
-	appUser := Initiate()
+	ctl := gomock.NewController(t)
+	db := mockdb.NewMockDBPort(ctl)
 	account := generateRandomUser()
-	appUser.RegisterUser(context.Background(), account)
+	shortPhoneNumber := utils.RandomePhoneNumber()
 	testCase := []struct {
 		name        string
 		phoneNumber string
@@ -148,34 +180,58 @@ func TestGetUserByPhoneNumber(t *testing.T) {
 		},
 		{
 			name:        "not found",
-			phoneNumber: utils.RandomUserName(),
+			phoneNumber: shortPhoneNumber,
 			checker: func(t *testing.T, user models.User, err models.Errors) {
 				require.Empty(t, user)
+				require.Error(t, err.Err)
+				require.EqualError(t, err.Err, sql.ErrNoRows.Error())
 
 			},
 		},
 		{
-			name:        "short phone number ",
+			name:        "short phone number",
 			phoneNumber: "2131",
 			checker: func(t *testing.T, user models.User, err models.Errors) {
 				require.Empty(t, user)
+				require.EqualError(t, err.Err, "the length must be exactly 13")
 
 			},
 		},
 	}
 	for _, tc := range testCase {
 		t.Run(tc.name, func(t *testing.T) {
-			accounts, err := appUser.GetUserByPhoneNumber(context.Background(), tc.phoneNumber)
-			tc.checker(t, accounts, err)
+
+			switch tc.name {
+			case "ok":
+				defer ctl.Finish()
+				db.EXPECT().GetUserByPhoneNumber(context.Background(), tc.phoneNumber).Return(account, models.Errors{})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUserByPhoneNumber(context.Background(), tc.phoneNumber)
+				tc.checker(t, accounts, err)
+			case "not found":
+				defer ctl.Finish()
+				db.EXPECT().GetUserByPhoneNumber(context.Background(), tc.phoneNumber).Return(models.User{}, models.Errors{Err: sql.ErrNoRows})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUserByPhoneNumber(context.Background(), tc.phoneNumber)
+				tc.checker(t, accounts, err)
+			case "short phone number":
+				defer ctl.Finish()
+				db.EXPECT().GetUserByPhoneNumber(context.Background(), tc.phoneNumber).Return(models.User{}, models.Errors{Err: errors.New("un expected error")})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUserByPhoneNumber(context.Background(), tc.phoneNumber)
+				tc.checker(t, accounts, err)
+			}
+
 		})
 	}
 
 }
 
 func TestGetUserByEmail(t *testing.T) {
-	appUser := Initiate()
+	ctl := gomock.NewController(t)
+	db := mockdb.NewMockDBPort(ctl)
 	account := generateRandomUser()
-	appUser.RegisterUser(context.Background(), account)
+
 	testCase := []struct {
 		name    string
 		email   string
@@ -198,22 +254,44 @@ func TestGetUserByEmail(t *testing.T) {
 			email: utils.RandomeEmail(),
 			checker: func(t *testing.T, user models.User, err models.Errors) {
 				require.Empty(t, user)
-
+				require.Error(t, err.Err)
+				require.EqualError(t, err.Err, sql.ErrNoRows.Error())
 			},
 		},
 		{
-			name:  "invalid email  ",
+			name:  "invalid email",
 			email: "2131.com",
 			checker: func(t *testing.T, user models.User, err models.Errors) {
 				require.Empty(t, user)
+				require.Error(t, err.Err)
+				require.EqualError(t, err.Err, "must be a valid email address")
 
 			},
 		},
 	}
 	for _, tc := range testCase {
 		t.Run(tc.name, func(t *testing.T) {
-			accounts, err := appUser.GetUserByEmail(context.Background(), tc.email)
-			tc.checker(t, accounts, err)
+			switch tc.name {
+			case "ok":
+				defer ctl.Finish()
+				db.EXPECT().GetUserByEmail(context.Background(), tc.email).Return(account, models.Errors{})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUserByEmail(context.Background(), tc.email)
+				tc.checker(t, accounts, err)
+			case "not found":
+				defer ctl.Finish()
+				db.EXPECT().GetUserByEmail(context.Background(), tc.email).Return(models.User{}, models.Errors{Err: sql.ErrNoRows})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUserByEmail(context.Background(), tc.email)
+				tc.checker(t, accounts, err)
+			case "invalid email":
+				defer ctl.Finish()
+				db.EXPECT().GetUserByEmail(context.Background(), tc.email).Return(models.User{}, models.Errors{Err: errors.New("no error")})
+				appUser := Initiate("../../../", db)
+				accounts, err := appUser.GetUserByEmail(context.Background(), tc.email)
+				tc.checker(t, accounts, err)
+			}
+
 		})
 	}
 
--- internal/module/user/fiterUser.go
+++ internal/module/user/fiterUser.go
@@ -2,16 +2,22 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"gitlab.com/2ftimeplc/2fbackend/delivery-1/internal/constants/models"
 )
 
+func errorHelper(name string, err error) error {
+
+	return fmt.Errorf("%s %s", name, err.Error())
+}
+
 func (s *service) GetUsersByFirstName(ctx context.Context, first_name string) ([]models.User, models.Errors) {
 	err := validation.Validate(first_name, validation.Required, validation.Length(2, 150))
 	if err != nil {
-		return []models.User{}, models.Errors{Err: err, ErrorLocation: "/internal/module/user/getusersbyfirstname.go", ErrLine: 96}
+		return []models.User{}, models.Errors{Err: errorHelper("first name", err), ErrorLocation: "/internal/module/user/getusersbyfirstname.go", ErrLine: 96}
 	}
 
 	accounst, errModel := s.databasAdapter.GetUsersByFirstName(ctx, first_name)
@@ -24,10 +30,10 @@ func (s *service) GetUsersByFirstName(ctx context.Context, first_name string) ([
 func (s *service) GetUsersByLastName(ctx context.Context, last_name string) ([]models.User, models.Errors) {
 	err := validation.Validate(last_name, validation.Required, validation.Length(2, 150))
 	if err != nil {
-		return []models.User{}, models.Errors{Err: err, ErrorLocation: "internal/storage/persistant/sqlc/Adapter.go", ErrLine: 96}
+		return []models.User{}, models.Errors{Err: errorHelper("LastName", err), ErrorLocation: "internal/storage/persistant/sqlc/Adapter.go", ErrLine: 96}
 	}
 
-	accounts, errModel := s.GetUsersByLastName(ctx, last_name)
+	accounts, errModel := s.databasAdapter.GetUsersByLastName(ctx, last_name)
 	if errModel.Err != nil {
 		return []models.User{}, errModel
 	}
